817e9fe1963d6ec3ee96921f98bb4129b6910373
GabrieleMeoni <gabriele.meoni@esa.int>
2022-07-28 13:49:36 +0000

    Updated notebook to change cfg params from default values. Added scale to cfg and net_builder. Minor corrections to comments in FixMatch.

--- MSMatch/utils/get_net_builder.py
+++ MSMatch/utils/get_net_builder.py
@@ -2,14 +2,14 @@ from efficientnet_pytorch import EfficientNet
 from ..models.nets.unet_encoder import UNetEncoder
 
 
-def get_net_builder(net_name, pretrained=False, in_channels=3):
+def get_net_builder(net_name, pretrained=False, in_channels=3, scale=1):
     """Creates the kind of neural network specified by net_name.
 
     Args:
         net_name (str): name of the network, can be efficientnet-b0, b1, b2, b3, b4, b5, b6, b7 or unet
         pretrained (bool, optional): if True, loads pretrained weights. Defaults to False.
         in_channels (int, optional): number of input channels. Defaults to 3.
-
+        scale (float, optional): scale of the network. Defaults to 1.0.
     Returns:
         torch.nn.Module: the created network
     """
@@ -30,7 +30,7 @@ def get_net_builder(net_name, pretrained=False, in_channels=3):
         assert in_channels == 3
         assert not pretrained
         return lambda num_classes, in_channels: UNetEncoder(
-            num_classes=num_classes, in_channels=in_channels, scale=1.0
+            num_classes=num_classes, in_channels=in_channels, scale=scale
         )
     else:
         assert Exception("Not Implemented Error")
