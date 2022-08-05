6c792c239b1de462cac1b11fe88b1865fad8e467
GabrieleMeoni <gabriele.meoni@esa.int>
2022-08-05 11:18:42 +0200

    Applied suggestions: moving get_default_cfg and print_cfg to load_cfg. Added missing functions to __init__, added comments and docstring.

--- notebooks/run_notebook.py
+++ notebooks/run_notebook.py
@@ -7,13 +7,13 @@ def main():
     parser.add_argument('--notebook_path', type=str, help="Path to the notebook file.", default="Baseline.ipynb")
     parser.add_argument('--cfg_path', type=str, help="Path to the cfg file.", default=None)
     parser.add_argument('--dataset', type=str, help="Dataset used for simulations.", default="eurosat_rgb")
-    parser.add_argument('--simulation_path', type=str, help="Path to simulation results.", default="saved_models/test")
+    parser.add_argument('--results_folder', type=str, help="Path to the results folder.", default="saved_models/test")
     pargs=parser.parse_args()
 
     result = pm.execute_notebook(
         input_path=pargs.notebook_path,
         output_path="/dev/null",
-        parameters={"cfg_path" : pargs.cfg_path, "dataset" : pargs.dataset, "simulation_path" : pargs.simulation_path},
+        parameters={"cfg_path" : pargs.cfg_path, "dataset" : pargs.dataset, "results_folder" : pargs.results_folder},
         kernel_name='distmsmatch', 
         log_output=True,
         autosave_cell_every=60,
